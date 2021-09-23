3380b2803d44ecf4953c91d81993ac6395f58bc7
Johannes Alves <johannes001002@gmail.com>
2021-09-23 16:46:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const FRONT = "card_front";
 const BACK = "card_back";
+const CARD = "card"
+const ICON = "icon";
 
 let techs =['bootstrap',
 'css',
 'electron',
-'firebasse',
+'firebase',
 'html',
 'javascript',
 'jquery',
@@ -12,19 +14,95 @@ let techs =['bootstrap',
 'node',
 'react'];
 
+let cards = null;
 
+startGame();
+
+
+function startGame(){
+    cards = createCardsFromTechs(techs);
+    shuffleCards(cards);
+    initializeCards(cards);
+}
+
+function initializeCards(cards){
+    let gameBoard = document.getElementById("gameBoard")
+ 
+        cards.forEach(card => {
+
+            let cardElement = document.createElement('div');
+            cardElement.id = card.id;
+            cardElement.classList.add(CARD);
+            cardElement.dataset.icon = card.icon;
+
+            createCardContent(card, cardElement);
+
+            cardElement.addEventListener('click', flipCard);
+            gameBoard.appendChild(cardElement);
+
+
+        })
+
+
+
+}
+
+function createCardContent(card, cardElement){
+
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+
+}
+
+function createCardFace(face, card, element){
+
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if(face === FRONT){
+
+        let iconElement = document.createElement('img')
+        iconElement.classList.add(ICON);
+        iconElement.src = "/images/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+
+    }else{
+        cardElementFace.innerHTML =  "&lt/&gt";
+    }
+    element.appendChild(cardElementFace);
+}
+
+
+// Embaralhas as cartas
+
+function shuffleCards(cards){
+    let currentIndex = cards.length;
+    let randomIndex = 0;
+
+    while(currentIndex !== 0){
+
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--;
+
+        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+    }
+
+}
+
+// Criar cartas á partir das techs
 createCardsFromTechs(techs);
 function createCardsFromTechs(techs){
 
     let cards = [];
 
-    for(let tech of techs){
+    techs.forEach((tech) => {
         cards.push(createPairFromTech(tech)); 
-    }
+    })
 
     return cards.flatMap(pair => pair);
 
 }
+
+// Par de cartas
 
 function createPairFromTech(tech){
 
@@ -40,7 +118,15 @@ function createPairFromTech(tech){
 
 
 }
-
+// Id criado com a intenção de saber qual carta está sendo clickada
 function createIdWithTech(tech){
     return tech + parseInt(Math.random() *1000);
+}
+
+// função de virar a carta
+
+function flipCard(){
+
+    this.classList.add("flip");
+
 }
